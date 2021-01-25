ed2d3a4c555496bad00ff58075738fc7e3fc751f
Robert (R.G.) Dinu <robert.dinu@ing.com>
2021-01-25 19:38:00 +0100

    Trying to fix path 2nd

--- scripts/fetch_kaggle_large_dataset.sh
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -7,5 +7,5 @@ kaggle datasets download -d $TRAINING_DATA_URL -p packages/neural_network_model/
 unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
 echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/neural_network_model/neural_network_model/datasets/training_data_reference.txt && \
 mkdir -p "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/nonsegmentedv2/Shepherds Purse"  && \
-mv -v "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/nonsegmentedv2/Shepherd’s Purse/"* "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/nonsegmentedv2/Shepherds Purse"
-rm -rf "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/nonsegmentedv2/Shepherd’s Purse"
+mv -v "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/nonsegmentedv2/Shepherd���s Purse/"* "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/nonsegmentedv2/Shepherds Purse"
+rm -rf "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/nonsegmentedv2/Shepherd���s Purse"
